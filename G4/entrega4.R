0d8053d151908afc7d98bf34a61a6644c9ecf190
Julián Fraga <60168622+julianfraga@users.noreply.github.com>
2022-09-05 21:17:43 -0300

    se agrega la sim. por método montecarlo
    Agregué una pequeña simulación en python y el CSV del set de datos que usé. Este CSV te lo da el mismo script de R y es un filtrado del CSV original con únicamente las entradas pertenecientes a la región 'southeast'

--- G4/entrega4.R
+++ G4/entrega4.R
@@ -7,51 +7,52 @@ df_seguros<-read.csv(file='D:/Documentos/UNSAM/Intro CDD/G4/Insurance.csv',dec="
 regiones <- unique(df_seguros['region']) #esto es un dataframe, ojo
 
 #un par de gráficos, uno para cada región
+df_seguros$smoker = as.factor(df_seguros$smoker)
+df_seguros$sex=as.factor(df_seguros$sex)
+df_seguros<-mutate(df_seguros, sobrepeso=bmi>=25) 
+df_seguros <- unique(df_seguros)
+glimpse(df_seguros)
+#bmi >25=sobrepeso
+
+#la lista de regiones
+regiones <- unique(df_seguros['region']) #esto es un dataframe, ojo
+
+#un par de gráficos, uno para cada región
 df_ne<-filter(df_seguros, region=='northeast')
-  ggplot(data = df_ne, aes(x=charges)) + 
+ggplot(data = df_ne, aes(x=charges)) + 
   geom_density_ridges(aes(y=smoker, color=sex), alpha=0.5)+
   labs(title="north-east",x ="Gasto anual")
 
+ggplot(data = df_ne, aes(x=charges)) + 
+  geom_density_ridges(aes(y = sobrepeso, color=sex), alpha=0.5)+
+  labs(title="north-east",x ="Gasto anual")
+
+
+
 df_se<-filter(df_seguros, region=='southeast')
-  ggplot(data = df_se, aes(x=charges)) + 
+ggplot(data = df_se, aes(x=charges)) + 
   geom_density_ridges(aes(y=smoker, color=sex), alpha=0.5)+
   labs(title="south-east",x ="Gasto anual")
 
 df_nw<-filter(df_seguros, region=='northwest')
-  ggplot(data = df_nw, aes(x=charges)) +
+ggplot(data = df_nw, aes(x=charges)) +
   geom_density_ridges(aes(y=smoker, color=sex), alpha=0.5)+
   labs(title="north-west",x ="Gasto anual")
 
 df_sw<-filter(df_seguros, region=='southwest')
-  ggplot(data = df_sw, aes(x=charges)) +
+ggplot(data = df_sw, aes(x=charges)) +
   geom_density_ridges(aes(y=smoker, color=sex), alpha=0.5)+
   labs(title="south-west",x ="Gasto anual")
 
 #Clave este Pipeline. Quiero métricas discriminadas por sexo para cada región
 metricas <-df_seguros%>% group_by(region, sex) %>% 
-    summarise(media=mean(charges),desviacion=sd(charges), mediana=median(charges), IQR=IQR(charges))
+  summarise(media=mean(charges),desviacion=sd(charges), mediana=median(charges), IQR=IQR(charges))
 
 metricas['desv_relativa']<-metricas['desviacion']/metricas['media']
 
 metricas%>%group_by(media, desv_relativa)%>%arrange(desc(media))
 
+write.csv(df_se,"D:/Documentos/UNSAM/Intro CDD/G4/insurance_SE.csv", row.names = FALSE)
 
 
-# cosas hechas en el labo-----
-
-df_sexo <- group_by(df_southeast, sex=='male')
-metrica_southeast <- summarise(df_sexo, mean(charges), median(charges))
-df_sexo_fumadores <- group_by(df_sexo, smoker=='yes')
-metrica_fumadores <- summarise(df_sexo_fumadores, mean(charges), median(charges))
-
-if (df_sexo_fumadores[sexbooleano]==TRUE){ #& df_sexo_fumadores['smoker=="yes"']==TRUE)
-  hombresfumadores <-summarise(df_sexo_fumadores, mean(charges), median(charges))}
-else if 
-  sex==FALSE and smoker==TRUE:
-  mujeresfumadoras <- summarise(df_sexo_fumadores, mean(charges), median(charges))
-else if 
-  sex==TRUE and smoker==FALSE:
-  hombresnofumadores <- summarise(df_sexo_fumadores, mean(charges), median(charges))
-else 
-  mujeresnofumadoras <- summarise(df_sexo_fumadores, mean(charges), median(charges))
 
